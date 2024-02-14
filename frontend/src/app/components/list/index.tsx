616450e5120e69a65f4dbc237e236abc6de9721a
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2024-02-14 08:45:11 -0300

    solved bug on pagination mobile

--- frontend/src/app/components/list/index.tsx
+++ frontend/src/app/components/list/index.tsx
@@ -87,20 +87,21 @@ const PaginationWidget = ({
       className="flex items-center justify-between bg-white px-4 py-3 sm:px-6"
     >
       <div className="flex flex-1 justify-between sm:hidden">
-        {current !== 0 && (
+        {current !== 1 ? (
           <a
             href={"/?page=" + (current - 1)}
             className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
           >
             Previous
           </a>
-        )}
+        ): <span />}
+         {current !== totalPages && (
         <a
           href={"/?page=" + (current + 1)}
           className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Next
-        </a>
+        </a>)}
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-end">
         <div>
